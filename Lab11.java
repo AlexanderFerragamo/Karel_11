64d501d75d9a158d65305338e2076c3de36741e8
AlexanderFerragamo <alexander_ferragamo@asl.org>
2014-12-07 17:34:07 +0000

    Lab11

--- Lab11.java
+++ Lab11.java
@@ -7,10 +7,33 @@
  */
 
 import edu.fcps.karel2.Display; 
+import edu.fcps.Digit;
 
 public class Lab11 {
 	 
 	 public static void main(String[] args) {
+    
+    Display.openWorld("maps/.map");
+   Display.setSize(50, 40);
+   Display.setSpeed(10);
+   
+   Digit Zero = new Zero (1,15);
+   Digit One = new One (4,15);
+   Digit Five = new Five (11,15);
+   Digit Two = new Two (18,15);
+   Digit Two2 = new Two (25,15);
+   Digit Eight = new Eight (32,15);
+   Digit Seven = new Seven (38,15);
+   Digit Five2 = new Five (45, 15);
+   
+   Zero.display();
+   One.display();
+   Five.display();
+   Two.display();
+   Two2.display();
+   Eight.display();
+   Seven.display();
+   Five2.display();
 	
 	 }
 	 
